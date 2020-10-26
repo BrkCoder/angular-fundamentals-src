98736c3ffa023fc83562c3c8f1707b960c5d4a49
BrkCoder <baribarakinbal@gmail.com>
2020-10-26 16:41:47 +0200

    :arrow_up: first-component - Angular 10

--- first-component/src/app/app.component.ts
+++ first-component/src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['app.component.scss'],
+  template: `
+    <div class="app">
+      {{ title }}
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'first-component';
+  title: string;
+  constructor() {
+    this.title = 'Ultimate Angular';
+  }
 }
--- first-component/src/app/app.module.ts
+++ first-component/src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
+  imports: [
+    BrowserModule,
+    CommonModule
+  ],
+  bootstrap: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {}
